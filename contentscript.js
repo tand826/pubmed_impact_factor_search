92637d01328313e0256255846ae199e37016140d
tand826 <takumi.ando826@gmail.com>
2021-01-15 19:28:37 +0900

    trigger input event to let the search box recognize input

--- contentscript.js
+++ contentscript.js
@@ -3,6 +3,9 @@ const addWords = additionalQuery => {
   let searchBox = document.getElementById("term") || document.getElementById("id_term");
   let baseQuery = searchBox.value;
   searchBox.value = baseQuery + " AND (" + additionalQuery["query"] + ")";
+  const evt = document.createEvent("HTMLEvents")
+  evt.initEvent("input", true, true)
+  searchBox.dispatchEvent(evt)
 };
 
 chrome.runtime.onMessage.addListener(addWords);
